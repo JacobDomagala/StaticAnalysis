a24dc5f0d1845579dffee4f60b5221c3f5568db7
Jakub Domagala <domagala.jacob@gmail.com>
2021-02-08 00:33:54 +0100

    Add feature to find comment_id based on comment author and title

--- dist/index.js
+++ dist/index.js
@@ -11,10 +11,7 @@ const github = __nccwpck_require__(308);
 async function main() {
   try {
     const result_in = core.getInput('compile_result');
-
     await create_or_update_comment(await find_comment_id(), process_compile_output(result_in));
-
-    core.setOutput("time", 0);
   } catch (error) {
     core.setFailed(error.message);
   }
@@ -45,27 +42,45 @@ function process_compile_output(compile_result) {
       file_line_end = (parseInt(file_line_start) + parseInt(core.getInput("num_lines_to_display"))).toString();
 
       // warning/error description
-      description = "```diff\n" + `-Line: ${file_line_start} ` + item.substring(item.indexOf(" ")) + "\n```\n";
+      description = "\n```diff\n" + `-Line: ${file_line_start} ` + item.substring(item.indexOf(" ")) + "\n```\n";
 
       // Concatinate both modified path to file and the description
-      var link_with_description = `https://github.com/${github.context.issue.owner}/${github.context.issue.repo}` +
-      `/blob/${github.context.sha}/${file_path}#L${file_line_start}-L${file_line_end} ${description} </br>`;
+      var link_with_description = `\nhttps://github.com/${github.context.issue.owner}/${github.context.issue.repo}` +
+        `/blob/${github.context.sha}/${file_path}#L${file_line_start}-L${file_line_end} ${description} </br>\n`;
 
       matchingStrings.push(link_with_description);
     }
   });
 
-  return matchingStrings.join('\n');
+  return `<details> <summary> <b> ${core.getInput("comment_title")} </b> </summary>\r\n${matchingStrings.join('\n')} </details>`;
+}
+
+function findCommentPredicate(comment) {
+  return comment.user.login == "github-actions[bot]" && comment.body.includes(core.getInput("comment_title"));
 }
 
 async function find_comment_id() {
   const octokit = github.getOctokit(core.getInput("token"));
 
-  await octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}/commits', {
+  const parameters = {
     owner: github.context.issue.owner,
     repo: github.context.issue.repo,
-    pull_number: core.getInput("pull_request_number")
-  });
+    issue_number: core.getInput("pull_request_number")
+  };
+
+  for await (const { data: comments } of octokit.paginate.iterator(
+    octokit.issues.listComments,
+    parameters
+  )) {
+    // Search each page for the comment
+    const comment = comments.find(comment =>
+      findCommentPredicate(comment)
+    )
+    if (comment) {
+      console.log(`Found comment! ID=${comment.id} user-login:${comment.user.login}`);
+      return comment.id;
+    }
+  };
 
   return -1;
 }
@@ -75,6 +90,7 @@ async function create_or_update_comment(comment_id, comment_body) {
 
   // Create comment
   if (comment_id == -1) {
+    console.log(`Adding new comment`);
     await octokit.issues.createComment({
       owner: github.context.issue.owner,
       repo: github.context.issue.repo,
@@ -84,6 +100,7 @@ async function create_or_update_comment(comment_id, comment_body) {
   }
   // Update comment
   else {
+    console.log(`Updating comment with ID=${comment_id} `);
     await octokit.issues.updateComment({
       owner: github.context.issue.owner,
       repo: github.context.issue.repo,
