c8c33b32f68ec304c948d952be6515f8fac6c28a
Jakub Domagala <domagala.jacob@gmail.com>
2021-04-24 18:49:02 +0200

    [#4]: Add Action input for additional apt packages

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,23 @@
 
 set -x
 
-cd "$GITHUB_WORKSPACE"
+if [ -n "$INPUT_APT_PCKGS" ]; then
+    for i in ${INPUT_APT_PCKGS//,/ }
+    do
+        apt-get install -y "$i"
+    done
+fi
 
-mkdir build && cd build
+cd "$GITHUB_WORKSPACE" || exit
+
+mkdir build && cd build || exit
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
 
 if [ -z "$INPUT_EXCLUDE_DIR" ]; then
     cppcheck src --enable=all --suppress=missingInclude --inline-suppr --inconclusive --output-file=cppcheck.txt --project=compile_commands.json
     run-clang-tidy >(tee "clang_tidy.txt")
 else
-    cppcheck src --enable=all --suppress=missingInclude --inline-suppr --inconclusive --output-file=cppcheck.txt --project=compile_commands.json -i$GITHUB_WORKSPACE/$INPUT_EXCLUDE_DIR
+    cppcheck src --enable=all --suppress=missingInclude --inline-suppr --inconclusive --output-file=cppcheck.txt --project=compile_commands.json -i"$GITHUB_WORKSPACE/$INPUT_EXCLUDE_DIR"
     run-clang-tidy "^((?!$GITHUB_WORKSPACE/$INPUT_EXCLUDE_DIR).)*$" > clang_tidy.txt
 fi
 
