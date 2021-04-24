409011776c2d8f7033a79895fda4d14176f92eee
Jakub Domagala <domagala.jacob@gmail.com>
2021-04-24 21:46:59 +0200

    [#1]: Add input variable to set CPPCHECK args

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,10 +19,10 @@ mkdir build && cd build || exit
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
 
 if [ -z "$INPUT_EXCLUDE_DIR" ]; then
-    cppcheck src --enable=all --suppress=missingInclude --inline-suppr --inconclusive --output-file=cppcheck.txt --project=compile_commands.json
+    cppcheck src "$INPUT_CPPCHECK_ARGS" --output-file=cppcheck.txt --project=compile_commands.json
     run-clang-tidy >(tee "clang_tidy.txt")
 else
-    cppcheck src --enable=all --suppress=missingInclude --inline-suppr --inconclusive --output-file=cppcheck.txt --project=compile_commands.json -i"$GITHUB_WORKSPACE/$INPUT_EXCLUDE_DIR"
+    cppcheck src "$INPUT_CPPCHECK_ARGS" --output-file=cppcheck.txt --project=compile_commands.json -i"$GITHUB_WORKSPACE/$INPUT_EXCLUDE_DIR"
     run-clang-tidy "^((?!$GITHUB_WORKSPACE/$INPUT_EXCLUDE_DIR).)*$" > clang_tidy.txt
 fi
 
