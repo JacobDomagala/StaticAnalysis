0e94cbf9be2d498545a324ce5576f152784e18ce
Jakub Domagala <domagala.jacob@gmail.com>
2021-07-04 17:24:20 +0200

    [#19]: Add python script to get all files for clang-tidy

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,7 @@ RUN chmod +x /entrypoint.sh
 COPY run_static_analysis.py /
 RUN chmod +x /run_static_analysis.py
 
+COPY get_files_to_check.py /
+RUN chmod +x /get_files_to_check.py
+
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -20,12 +20,14 @@ fi
 mkdir build && cd build || exit
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON "$INPUT_CMAKE_ARGS" ..
 
+files_to_check=$(python3 /get_files_to_check.py -exclude="$INPUT_EXCLUDE_DIR" -json="compile_commands.json")
+
 if [ -z "$INPUT_EXCLUDE_DIR" ]; then
     eval cppcheck --project=compile_commands.json "$INPUT_CPPCHECK_ARGS" --output-file=cppcheck.txt
-    eval clang-tidy-12 -p="$(pwd)" "$INPUT_CLANG_TIDY_ARGS" -- >(tee "clang_tidy.txt")
+    eval clang-tidy-12 "$INPUT_CLANG_TIDY_ARGS" -p="$(pwd)" "$files_to_check" -- >(tee "clang_tidy.txt")
 else
     eval cppcheck --project=compile_commands.json "$INPUT_CPPCHECK_ARGS" --output-file=cppcheck.txt -i"$GITHUB_WORKSPACE/$INPUT_EXCLUDE_DIR"
-    clang-tidy-12 -p="$(pwd)" "$INPUT_CLANG_TIDY_ARGS" "^((?!$GITHUB_WORKSPACE/$INPUT_EXCLUDE_DIR).)*$" -- > clang_tidy.txt
+    clang-tidy-12 "$INPUT_CLANG_TIDY_ARGS" -p $(pwd) "$files_to_check" -- > clang_tidy.txt
 fi
 
 python3 /run_static_analysis.py -cc cppcheck.txt -ct clang_tidy.txt
