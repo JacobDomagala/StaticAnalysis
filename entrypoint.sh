667a2a89d6b176c783da9b759c6f5cc911ef8b19
Jakub Domagala <domagala.jacob@gmail.com>
2021-07-04 15:46:00 +0200

    [#19]: Update clang-tidy args

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,10 +22,10 @@ cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON "$INPUT_CMAKE_ARGS" ..
 
 if [ -z "$INPUT_EXCLUDE_DIR" ]; then
     eval cppcheck --project=compile_commands.json "$INPUT_CPPCHECK_ARGS" --output-file=cppcheck.txt
-    run-clang-tidy-12 -- >(tee "clang_tidy.txt")
+    run-clang-tidy-12 -quiet -- >(tee "clang_tidy.txt")
 else
     eval cppcheck --project=compile_commands.json "$INPUT_CPPCHECK_ARGS" --output-file=cppcheck.txt -i"$GITHUB_WORKSPACE/$INPUT_EXCLUDE_DIR"
-    run-clang-tidy-12 "^((?!$GITHUB_WORKSPACE/$INPUT_EXCLUDE_DIR).)*$" -- > clang_tidy.txt
+    run-clang-tidy-12 -quiet "^((?!$GITHUB_WORKSPACE/$INPUT_EXCLUDE_DIR).)*$" -- > clang_tidy.txt
 fi
 
 python3 /run_static_analysis.py -cc cppcheck.txt -ct clang_tidy.txt
