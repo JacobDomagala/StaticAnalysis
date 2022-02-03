20952b29a57e629cbcb8a09f4f2b3b3ea33c1d76
Jacob Domagala <domagala.jacob@gmail.com>
2022-02-03 03:07:45 +0100

    [#37]: Linter issues

--- get_files_to_check.py
+++ get_files_to_check.py
@@ -7,9 +7,11 @@ parser.add_argument("-dir", help="Current directory", required=True)
 
 directory = parser.parse_args().dir
 exclude_prefixes = [parser.parse_args().exclude, f"{directory}/build"]
-supported_extensions = [ ".h", ".hpp", ".hcc", ".c", ".cc", ".cpp", ".cxx"]
+supported_extensions = [".h", ".hpp", ".hcc", ".c", ".cc", ".cpp", ".cxx"]
 
-for path in Path(directory).rglob('*.*'):
-    p = str(path.resolve())
-    if p.endswith(tuple(supported_extensions)) and not p.startswith(tuple(exclude_prefixes)):
-        print(p)
+for path in Path(directory).rglob("*.*"):
+    PATH = str(path.resolve())
+    if PATH.endswith(tuple(supported_extensions)) and not PATH.startswith(
+        tuple(exclude_prefixes)
+    ):
+        print(PATH)
