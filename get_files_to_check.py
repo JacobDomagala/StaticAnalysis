99c35124b58600c11affac50f9e482383085c057
Jacob Domagala <domagala.jacob@gmail.com>
2022-02-05 17:12:27 +0100

    [#37]: Avoid unnecessary cast to tuple

--- get_files_to_check.py
+++ get_files_to_check.py
@@ -9,12 +9,12 @@ directory = parser.parse_args().dir
 exclude_prefixes = [f"{directory}/build"]
 if parser.parse_args().exclude:
     exclude_prefixes.append(str(parser.parse_args().exclude))
-supported_extensions = [".h", ".hpp", ".hcc", ".c", ".cc", ".cpp", ".cxx"]
+supported_extensions = tuple(".h", ".hpp", ".hcc", ".c", ".cc", ".cpp", ".cxx")
 all_files = []
 
 for path in Path(directory).rglob("*.*"):
     PATH = str(path.resolve())
-    if PATH.endswith(tuple(supported_extensions)) and not PATH.startswith(
+    if PATH.endswith(supported_extensions) and not PATH.startswith(
         tuple(exclude_prefixes)
     ):
         all_files.append(PATH)
