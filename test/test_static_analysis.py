9c5027d3d31903d745cecfb5bdf5954424ba81e7
Jacob Domagala <domagala.jacob@gmail.com>
2022-10-27 00:48:27 +0200

    [#39]: Initial version for test_create_comment_for_output

--- test/test_static_analysis.py
+++ test/test_static_analysis.py
@@ -1,6 +1,7 @@
 import unittest
 import os
 import sys
+
 try:
     project_path = f"{os.sep}".join(os.path.abspath(__file__).split(os.sep)[:-2])
     sys.path.append(project_path)
@@ -8,12 +9,52 @@ except Exception as e:
     print(f"Can not add project path to system path! Exiting!\nERROR: {e}")
     raise SystemExit(1)
 
+os.environ["GITHUB_WORKSPACE"] = f"{project_path}/test/utils"
+os.environ["INPUT_VERBOSE"] = "True"
+os.environ["INPUT_REPORT_PR_CHANGES_ONLY"] = "False"
+os.environ["INPUT_REPO"] = "RepoName"
+os.environ["GITHUB_SHA"] = "1234"
+
 from src import run_static_analysis
 
+
 class TestStringMethods(unittest.TestCase):
+    def test_create_comment_for_output(self):
 
-    def test_get_lines_changed_from_patch(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+        cppcheck_content = [
+            "/github/workspace/DummyFile.cpp:8:23: style: Error message\n",
+            "    Part of code\n",
+            "               ^\n",
+            "/github/workspace/DummyFile.cpp:6:12: note: Note message\n",
+            "    Part of code\n",
+            "               ^\n",
+            "/github/workspace/DummyFile.cpp:3:0: style: Error message\n",
+            "    Part of code\n",
+            "               ^\n",
+        ]
 
-if __name__ == '__main__':
+        files_changed_in_pr = {
+            "/github/workspace/DummyFile.hpp": ("added", (1, 10)),
+            "/github/workspace/DummyFile.cpp": ("added", (1, 10)),
+        }
+        result = run_static_analysis.create_comment_for_output(
+            cppcheck_content, "/github/workspace", files_changed_in_pr, False
+        )
+
+        SHA = os.getenv("GITHUB_SHA")
+        REPO_NAME = os.getenv("INPUT_REPO")
+        expected = (
+            f"\n\nhttps://github.com/{REPO_NAME}/blob/{SHA}/DummyFile.cpp#L8-L9 \n"
+            f"```diff\n!Line: 8 - style: Error message\n``` \n <br>"
+            f"\n\n```diff\n!Line: 6 - note: Note message\n``` "
+            f"\n\n\nhttps://github.com/{REPO_NAME}/blob/{SHA}/DummyFile.cpp#L3-L8 \n"
+            f"```diff\n!Line: 3 - style: Error message\n``` \n <br>\n"
+        )
+
+        print(result)
+
+        self.assertEqual(result, (expected, 2))
+
+
+if __name__ == "__main__":
     unittest.main()
---
+++ test/utils/DummyFile.cpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+
+int SomeFunction(int){
+    return 1;
+}
+
+int main(int, char**){
+    int some_variable = 4;
+}
