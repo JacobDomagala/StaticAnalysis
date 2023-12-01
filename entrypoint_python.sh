416c4fe8574e9759a022e90bf1f749f42954c763
Jacob Domagala <domagala.jacob@gmail.com>
2023-12-01 20:42:24 +0100

    [#112]: Fix issues with PyLint

--- entrypoint_python.sh
+++ entrypoint_python.sh
@@ -9,6 +9,11 @@ print_to_console=${print_to_console:-false}
 use_extra_directory=${use_extra_directory:-false}
 common_ancestor=${common_ancestor:-""}
 
+if [ -z "$INPUT_PYTHON_DIRS" ]; then
+    debug_print "Error: python_dirs action input is empty! You have to provide directories that contain python files to be checked."
+    exit 1
+fi
+
 if [ -z "$INPUT_EXCLUDE_DIR" ]; then
     files_to_check=$(python3 /get_files_to_check.py -dir="$GITHUB_WORKSPACE" -preselected="$preselected_files" -lang="python")
     debug_print "Running: files_to_check=python3 /get_files_to_check.py -dir=\"$GITHUB_WORKSPACE\" -preselected=\"$preselected_files\")"
@@ -26,6 +31,6 @@ else
 
     # Trim newlines
     INPUT_PYLINT_ARGS="${INPUT_PYLINT_ARGS%"${INPUT_PYLINT_ARGS##*[![:space:]]}"}"
-    eval "pylint . --output-format=json:pylint_out.json ${INPUT_PYLINT_ARGS}|| true"
+    eval "pylint $INPUT_PYTHON_DIRS --output-format=json:pylint_out.json $INPUT_PYLINT_ARGS || true"
     python3 /python/run_static_analysis.py -pl ./pylint_out.json -o "$print_to_console" -fk "$use_extra_directory" --common "$common_ancestor" --head "origin/$GITHUB_HEAD_REF"
 fi
